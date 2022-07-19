c55d88292842bb81a4f06053093119cb6614c53e
sokritha <sokritha17@gmail.com>
2022-07-19 16:25:38 +0700

    initialize passport

--- api/app.js
+++ api/app.js
@@ -7,13 +7,13 @@ const authRouter = require("./routes/auth.routes");
 const app = express();
 
 // Global Middleware
-
-app.use(passport.initialize());
 app.use(cors());
 // Body parser ()
 // (convert the body from post request to json[express.json()] + html post form to json[express.urlencoded()])
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+app.use(passport.initialize());
 
 // Routes
 app.use("/api/v1/auth/facebook", authRouter);
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
+  console.log(req);
   res.status(200).json({
     status: "success",
     data: null,
