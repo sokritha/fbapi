aeaf208168d22de683527760515e536e4374a92a
sokritha <sokritha17@gmail.com>
2022-07-20 05:12:01 +0700

    change axios method on button fb login to href

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,25 +1,16 @@
 import styles from "../styles/signin.module.css";
 import { TextField, Box } from "@mui/material";
-import { Button } from "@components/commons";
+import { Button, Link } from "@components/commons";
 import axios from "axios";
 
 const SERVER_URL = "https://fbapi-backend.herokuapp.com/api/v1";
 
 export default function Signin() {
-  const handleOnLogin = async () => {
-    try {
-      const response = await axios.get(`${SERVER_URL}/auth/facebook`);
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <div className={styles.container}>
-      <Button color="blue" variant="contained" handleOnAction={handleOnLogin}>
-        Facebook Login
-      </Button>
+      <Link href={`${SERVER_URL}/auth/facebook`}>
+        <a>Facebook Login</a>
+      </Link>
     </div>
   );
 }
