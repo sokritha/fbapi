ae0a6930013fa177dfdc1d02527ad659f217ca6f
sokritha <sokritha17@gmail.com>
2022-07-20 14:09:29 +0700

    fixed jwt

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -40,11 +40,20 @@ exports.loginFacbook = (req, res, next) => {
         process.env.BASE_CLIENT_URL + "/login?error=" + info.message
       );
     }
-    req.login(user, { session: false }, (err) => {
+    req.login(user, { session: false }, async (err) => {
       if (err) {
         res.status(400).send({ err });
       }
-      createSendToken(user.facebookId);
+      const token = await signToken(user.id);
+
+      const cookieOptions = {
+        expires: new Date(
+          Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true, // cookie cannot be accessed or modified through browser
+      };
+
+      res.cookie("jwt_token", token, cookieOptions);
       res.redirect(process.env.BASE_CLIENT_URL + "/dashboard");
     });
   })(req, res, next);
